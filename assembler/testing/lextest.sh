86919cc3450f52727bfa45797ae75c0ee282b381
Gareth Richardson <grich_86@yahoo.com.au>
2012-10-23 00:14:14 +1100

    Adding lex testing bash script.

---
+++ assembler/testing/lextest.sh
@@ -0,0 +1,11 @@
+g++ ../lex/tokenlist.cpp -c
+g++ ../lex/lex.cpp -c
+g++ ../character/characterlist.cpp -c
+g++ testlex.cpp -c
+g++ tokenlist.o lex.o characterlist.o testlex.o -o unittest
+./unittest
+rm tokenlist.o
+rm lex.o
+rm characterlist.o
+rm testlex.o
+rm unittest.exe

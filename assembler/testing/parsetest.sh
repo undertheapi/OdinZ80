5981e6aeef883d091f8772fbdb264d1fbfe4ed7f
undertheapi <garethrichardson86@gmail.com>
2013-03-11 18:01:15 +1100

    Adding lines for compiling the instructions

--- assembler/testing/parsetest.sh
+++ assembler/testing/parsetest.sh
@@ -20,6 +20,37 @@ g++ ../parser/instructions/XORinstr.cpp -c
 g++ ../parser/instructions/CPinstr.cpp -c
 g++ ../parser/instructions/INCinstr.cpp -c
 g++ ../parser/instructions/DECinstr.cpp -c
+g++ assembler/parser/instructions/LDinstr.cpp -c
+g++ assembler/parser/instructions/PUSHinstr.cpp -c
+g++ assembler/parser/instructions/POPinstr.cpp -c
+g++ assembler/parser/instructions/EXinstr.cpp -c
+g++ assembler/parser/instructions/ADDinstr.cpp -c
+g++ assembler/parser/instructions/ADCinstr.cpp -c
+g++ assembler/parser/instructions/SUBinstr.cpp -c
+g++ assembler/parser/instructions/SUBCinstr.cpp -c
+g++ assembler/parser/instructions/ANDinstr.cpp -c
+g++ assembler/parser/instructions/ORinstr.cpp -c
+g++ assembler/parser/instructions/XORinstr.cpp -c
+g++ assembler/parser/instructions/CPinstr.cpp -c
+g++ assembler/parser/instructions/INCinstr.cpp -c
+g++ assembler/parser/instructions/DECinstr.cpp -c
+g++ assembler/parser/instructions/RLCinstr.cpp -c
+g++ assembler/parser/instructions/RLinstr.cpp -c
+g++ assembler/parser/instructions/RRCinstr.cpp -c
+g++ assembler/parser/instructions/RRinstr.cpp -c
+g++ assembler/parser/instructions/SLAinstr.cpp -c
+g++ assembler/parser/instructions/SRAinstr.cpp -c
+g++ assembler/parser/instructions/SRLinstr.cpp -c
+g++ assembler/parser/instructions/BITinstr.cpp -c
+g++ assembler/parser/instructions/SETinstr.cpp -c
+g++ assembler/parser/instructions/RESinstr.cpp -c
+g++ assembler/parser/instructions/JPinstr.cpp -c
+g++ assembler/parser/instructions/JRinstr.cpp -c
+g++ assembler/parser/instructions/CALLinstr.cpp -c
+g++ assembler/parser/instructions/RETinstr.cpp -c
+g++ assembler/parser/instructions/RSTinstr.cpp -c
+g++ assembler/parser/instructions/INinstr.cpp -c
+g++ assembler/parser/instructions/OUTinstr.cpp -c
 g++ DECinstr.o INCinstr.o XORinstr.o CPinstr.o characterlist.o tokenlist.o lex.o bytecode.o parser.o addresslist.o foundlist.o testparser.o LDinstr.o PUSHinstr.o POPinstr.o EXinstr.o ADDinstr.o ADCinstr.o SUBinstr.o SUBCinstr.o ANDinstr.o ORinstr.o -o unittest
 ./unittest
 rm characterlist.o 

9fa73963e9cab38dde91c6bfe5abbe30bde03372
Gareth Richardson <grich_86@yahoo.com.au>
2013-10-21 21:09:27 +1100

    Adding more register definitions to the header

--- debugger/registers/registers.hpp
+++ debugger/registers/registers.hpp
@@ -1,6 +1,74 @@
+/*
+	Copyright (c) 2013, Gareth Richardson
+	All rights reserved.
+
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met:
+
+	1. Redistributions of source code must retain the above copyright notice, this
+	   list of conditions and the following disclaimer.
+	2. Redistributions in binary form must reproduce the above copyright notice,
+	   this list of conditions and the following disclaimer in the documentation
+	   and/or other materials provided with the distribution.
+
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+	DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+	ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+	ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+	The views and conclusions contained in the software and documentation are those
+	of the authors and should not be interpreted as representing official policies,
+	either expressed or implied, of the FreeBSD Project.
+*/
+
+/*
+	file name: registers.hpp
+	date created: 21/10/2013
+	date updated: 21/10/2013
+	author: Gareth Richardson
+	description: The header file for the registers object
+*/
+
+/*
+	8 BIT register definitions:
+*/
+#define REGISTER8 unsigned char
+#define REG_A 7
+#define REG_B 0
+#define REG_C 1
+#define REG_D 2
+#define REG_E 3
+#define REG_H 4
+#define REG_L 5
+
+/*
+	16 BIT register definitions:
+*/
+#define REGISTER16 unsigned char
+#define REG_BC 0
+#define REG_DE 1
+#define REG_HL 2
+#define REG_SP 3
+
 class Registers {
   private:
     unsigned char[8] registerArray;
+    unsigned short IP;
+    unsigned short IX;
+    unsigned short IY;
   public:
-    
+  	unsigned short getIP();
+  	void incrementIP();
+  	
+  	unsigned short getIX();
+  	unsigned short getIY();
+  	
+    void load8Bit(REGISTER8 reg1, REGISTER8 reg2);
+    void load8BitImm(REGISTER8 reg, unsigned char value);
 };
